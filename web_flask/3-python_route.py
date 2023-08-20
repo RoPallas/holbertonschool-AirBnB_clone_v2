a866f5f2216c527d78140e0a245192b6f5d05cc6
RoPallas <rocio.orquestas@gmail.com>
2023-08-20 08:20:07 -0700

    Fix PEP8 errors

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,26 +4,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """function to display a string"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """function to display a string"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """function to display a string"""
     return f"C {text.replace('_', ' ')}"
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_text(text="is cool"):
     """function to display a string"""
     return f"Python {text.replace('_', ' ')}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
